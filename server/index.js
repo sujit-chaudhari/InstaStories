bc5dfcc73c6eb1a627193e277f9dd5516247ebb2
sujit-chaudhari <sujit.chaudhari@live.in>
2022-10-02 21:07:48 +0530

    set up atlas database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://memoriesapp:8J1I1XQQzl4aq6cU@cluster0.6yryjfx.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
