97acc79229d6a03a7d127cceb3251ec08a390858
sujit-chaudhari <sujit.chaudhari@live.in>
2022-10-10 20:57:11 +0530

    convert upload image to base64

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-
+import FileBase from "react-file-base64";
 import useStyles from "./styles";
 const Form = () => {
   const [postData, setPostData] = useState({
@@ -58,6 +58,15 @@ const Form = () => {
           value={postData.tags}
           onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
         />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
       </form>
     </Paper>
   );
