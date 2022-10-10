f6435f600477e90116fcc7bbffc2bc18303703a0
sujit-chaudhari <sujit.chaudhari@live.in>
2022-10-10 14:58:00 +0530

    Added material ui styles for Post.js, Posts.js and Form.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,22 @@ import memories from "./images/memories.png";
 import Post from "./components/Posts/Post/Post";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
-import useStyles from "/.styles";
+import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
   return (
     <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
