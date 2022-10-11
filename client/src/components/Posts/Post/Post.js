20d810752368069ba7cebf8837b2018b25b0d774
sujit-chaudhari <sujit.chaudhari@live.in>
2022-10-11 16:35:58 +0530

    added svgbackground css to index.css

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -25,7 +25,7 @@ const Post = ({ post }) => {
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
         <Typography variant="body2">
-          {moment(post.creator).fromNow()}
+          {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
       <div className={classes.overlay2}>
