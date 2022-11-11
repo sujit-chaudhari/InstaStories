2522bdd1e78d609c3c97105042498ed8d2edb1e8
sujit-chaudhari <sujit.chaudhari@live.in>
2022-11-11 12:51:20 +0530

    Added Auth component for Authorisation

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Avatar, Typography, Button } from "@material-ui/core";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
 
 const Navbar = () => {
   const classes = useStyles();
+  const user = null;
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
@@ -25,6 +26,39 @@ const Navbar = () => {
         alt="memories"
         height="60"
       />
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className="className.logout"
+              color="secondary"
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            {" "}
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
